9e3cdf734ff49e556812dba47c64e437bf117321
Giovanna Oliveira Pinto Passos da Silva <giovanna.passos@labs.dcx>
2023-06-01 11:24:27 -0300

    hello world giovanna

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Davi Icaro, Helena Bispo");
+        System.out.println("Hello world! Davi Icaro, Helena Bispo e giovanna passos");
     }
 }
